f8db81201ecfd14b0b066c32fc406b19e7f5b3e3
Varun Harinath <varunharinath15@gmail.com>
2025-12-07 23:08:13 -0600

    updated

--- lib/screens/event_detail_screen.dart
+++ lib/screens/event_detail_screen.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 
 class EventDetailScreen extends StatelessWidget {
   final Map<String, String> event;
-  EventDetailScreen({required this.event});
+  final VoidCallback? onRSVP;
+
+  EventDetailScreen({required this.event, this.onRSVP});
 
   @override
   Widget build(BuildContext context) {
@@ -20,20 +22,18 @@ class EventDetailScreen extends StatelessWidget {
               ),
             ),
           ),
-          elevation: 4,
         ),
       ),
       body: SingleChildScrollView(
         child: Column(
           children: [
-            event['image']!.isNotEmpty
-                ? Image.network(
-                    event['image']!,
-                    height: 250,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  )
-                : Container(height: 250, color: Colors.grey.shade300),
+            if (event['image'] != null && event['image']!.isNotEmpty)
+              Image.network(
+                event['image']!,
+                fit: BoxFit.cover,
+                height: 250,
+                width: double.infinity,
+              ),
             Padding(
               padding: EdgeInsets.all(16),
               child: Column(
@@ -45,18 +45,21 @@ class EventDetailScreen extends StatelessWidget {
                   ),
                   SizedBox(height: 8),
                   Text(
-                    event['date']!,
+                    event['date'] ?? '',
                     style: TextStyle(color: Colors.grey, fontSize: 16),
                   ),
                   SizedBox(height: 16),
                   Text(
-                    'Detailed description of the event goes here.',
+                    'This is a detailed description of the event. Add more info like speakers, location, timing.',
                     style: TextStyle(fontSize: 16),
                   ),
                   SizedBox(height: 20),
                   Center(
                     child: ElevatedButton(
-                      onPressed: () {},
+                      onPressed: () {
+                        if (onRSVP != null) onRSVP!();
+                        Navigator.pop(context);
+                      },
                       child: Text('RSVP'),
                       style: ElevatedButton.styleFrom(
                         padding: EdgeInsets.symmetric(
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'event_detail_screen.dart';
 import 'rsvp_form_dialog.dart';
+import 'past_events_screen.dart';
 
 class HomeScreen extends StatefulWidget {
   final List<Map<String, String>> pastEvents;
+
   HomeScreen({required this.pastEvents});
 
   @override
@@ -20,21 +22,23 @@ class _HomeScreenState extends State<HomeScreen> {
       "organizer": "Flutter Devs",
       "description":
           "Hands-on workshop to learn Flutter and build beautiful mobile apps.",
-      "image": "",
+      "image": "", // leave empty for online handling
     },
   ];
-
-  void addEvent(Map<String, String> newEvent) {
-    setState(() {
-      events.add(newEvent);
-    });
-  }
 
   void moveToPast(Map<String, String> event) {
     setState(() {
       events.remove(event);
       widget.pastEvents.add(event);
     });
+  }
+
+  void handleRSVP(Map<String, String> event) async {
+    final rsvp = await showDialog<Map<String, String>>(
+      context: context,
+      builder: (_) => RSVPFormDialog(event: event),
+    );
+    if (rsvp != null) moveToPast(event);
   }
 
   @override
@@ -59,9 +63,13 @@ class _HomeScreenState extends State<HomeScreen> {
         onPressed: () async {
           final newEvent = await showDialog<Map<String, String>>(
             context: context,
-            builder: (_) => EventFormDialog(),
+            builder: (_) => RSVPFormDialog(event: {}),
           );
-          if (newEvent != null) addEvent(newEvent);
+          if (newEvent != null) {
+            setState(() {
+              events.add(newEvent);
+            });
+          }
         },
         child: Icon(Icons.add),
         backgroundColor: Colors.deepPurpleAccent,
@@ -77,7 +85,10 @@ class _HomeScreenState extends State<HomeScreen> {
                 Navigator.push(
                   context,
                   MaterialPageRoute(
-                    builder: (_) => EventDetailScreen(event: event),
+                    builder: (_) => EventDetailScreen(
+                      event: event,
+                      onRSVP: () => handleRSVP(event),
+                    ),
                   ),
                 );
               },
@@ -88,117 +99,115 @@ class _HomeScreenState extends State<HomeScreen> {
                   borderRadius: BorderRadius.circular(20),
                 ),
                 shadowColor: Colors.deepPurpleAccent.withOpacity(0.3),
-                child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        event['title']!,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    if (event['image'] != null && event['image']!.isNotEmpty)
+                      ClipRRect(
+                        borderRadius: BorderRadius.vertical(
+                          top: Radius.circular(20),
+                        ),
+                        child: Image.network(
+                          event['image']!,
+                          height: 180,
+                          width: double.infinity,
+                          fit: BoxFit.cover,
                         ),
                       ),
-                      SizedBox(height: 8),
-                      Text(event['date']! + ' | ' + event['time']!),
-                      SizedBox(height: 4),
-                      Text(event['location']!),
-                      SizedBox(height: 12),
-                      Text(event['description']!),
-                      SizedBox(height: 16),
-                      Center(
-                        child: ElevatedButton(
-                          onPressed: () async {
-                            final rsvp = await showDialog(
-                              context: context,
-                              builder: (_) => RSVPFormDialog(event: event),
-                            );
-                            if (rsvp != null) moveToPast(event);
-                          },
-                          child: Text('RSVP'),
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.deepPurpleAccent,
+                    Padding(
+                      padding: EdgeInsets.all(16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            event['title']!,
+                            style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
                           ),
-                        ),
+                          SizedBox(height: 8),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.calendar_today,
+                                size: 16,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                "${event['date']} | ${event['time']}",
+                                style: TextStyle(
+                                  color: Colors.grey[700],
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 4),
+                          Row(
+                            children: [
+                              Icon(
+                                Icons.location_on,
+                                size: 16,
+                                color: Colors.grey,
+                              ),
+                              SizedBox(width: 4),
+                              Text(
+                                event['location'] ?? '',
+                                style: TextStyle(
+                                  color: Colors.grey[700],
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 4),
+                          Row(
+                            children: [
+                              Icon(Icons.person, size: 16, color: Colors.grey),
+                              SizedBox(width: 4),
+                              Text(
+                                event['organizer'] ?? '',
+                                style: TextStyle(
+                                  color: Colors.grey[700],
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                          SizedBox(height: 12),
+                          Text(
+                            event['description'] ?? '',
+                            style: TextStyle(fontSize: 14),
+                          ),
+                          SizedBox(height: 16),
+                          Center(
+                            child: ElevatedButton(
+                              onPressed: () => handleRSVP(event),
+                              child: Text('RSVP'),
+                              style: ElevatedButton.styleFrom(
+                                padding: EdgeInsets.symmetric(
+                                  horizontal: 40,
+                                  vertical: 14,
+                                ),
+                                shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                backgroundColor: Colors.deepPurpleAccent,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             );
           },
         ),
       ),
-    );
-  }
-}
-
-// ---------------- Event Form Dialog ------------------
-
-class EventFormDialog extends StatefulWidget {
-  @override
-  _EventFormDialogState createState() => _EventFormDialogState();
-}
-
-class _EventFormDialogState extends State<EventFormDialog> {
-  final _formKey = GlobalKey<FormState>();
-  final Map<String, String> eventData = {};
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text('Publish Event'),
-      content: SingleChildScrollView(
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: [
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Title'),
-                onSaved: (val) => eventData['title'] = val ?? '',
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Date'),
-                onSaved: (val) => eventData['date'] = val ?? '',
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Time'),
-                onSaved: (val) => eventData['time'] = val ?? '',
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Location'),
-                onSaved: (val) => eventData['location'] = val ?? '',
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Organizer'),
-                onSaved: (val) => eventData['organizer'] = val ?? '',
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Description'),
-                onSaved: (val) => eventData['description'] = val ?? '',
-              ),
-              TextFormField(
-                decoration: InputDecoration(labelText: 'Image URL'),
-                onSaved: (val) => eventData['image'] = val ?? '',
-              ),
-            ],
-          ),
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () => Navigator.pop(context, null),
-          child: Text('Cancel'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            _formKey.currentState!.save();
-            Navigator.pop(context, eventData);
-          },
-          child: Text('Publish'),
-        ),
-      ],
     );
   }
 }
